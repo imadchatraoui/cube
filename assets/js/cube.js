366daabff0b8c6df195d3875f769567163d3326c
Imad Chatraoui <imad.chatraoui@gmail.com>
2026-06-26 16:09:29 +0200

    Update color palette for 'kukko' and increment game version to 1.0.1

--- assets/js/cube.js
+++ assets/js/cube.js
@@ -3256,16 +3256,16 @@
 	        P: 0x08101a, // piece
 	        G: 0xd1d5db, // background
 	      },
-	      kukko: {
-	        U: 0xffffff,
-	        D: 0xffd500,
-	        F: 0xc41e3a,
-	        R: 0x0051ba,
-	        B: 0xff5800,
-	        L: 0x009e60,
-	        P: 0x08101a,
-	        G: 0x8abdff,
-	      },
+		  kukko: {
+            U: 0xf8f9fa, // panna / bianco pastello
+            D: 0xfdfd96, // giallo pastello
+            F: 0xff6961, // rosso pastello
+            R: 0xaec6cf, // blu pastello
+            B: 0xffb347, // arancione pastello
+            L: 0x77dd77, // verde pastello
+            P: 0xffffff, // plastica interna (bianca per risaltare i pastelli)
+            G: 0xf1f5f9, // sfondo (grigio-azzurrino chiarissimo, perfetto per i pastelli)
+          },
 	      dust: {
 	        U: 0xfff6eb,
 	        D: 0xe7c48d,
